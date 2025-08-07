2a3362c92f72dc33d480b4a95a1fe6871dc92050
Ahmet Oğuz Azık <ahmetoguzazik2005@gmail.com>
2025-08-07 05:49:29 +0300

    second main commit
    second main commit

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
-import java.util.Scanner;
+import javax.swing.*;
+
 
 public class Main {
+
+    static boolean fullScreen = false;
+
+
     public static void main(String[] args) {
+        MyFrame myFrame = new MyFrame();
+        myFrame.setTitle("No Limit"); // Konusuruz ismi
 
-
-
+        // Main Panel - will be cont.
+        JPanel panel = new JPanel();
+        myFrame.add(panel);
     }
+
+
 }
---
+++ MyFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+    MyFrame() {
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        // Normal size at the start
+        this.setSize(800, 600);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+
+    }
+
+}
